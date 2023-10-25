ccdd77a257bf327d5b89bcafb505b6f0959a8d63
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2023-10-25 02:20:13 +0000

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -8,11 +8,10 @@
  */
 void swap(int *array, ssize_t item1, ssize_t item2)
 {
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
+    int tmp;
+    tmp = array[item1];
+    array[item1] = array[item2];
+    array[item2] = tmp;
 }
 
 /**
@@ -25,27 +24,27 @@ void swap(int *array, ssize_t item1, ssize_t item2)
  */
 int hoare_partition(int *array, int first, int last, int size)
 {
-	int pivot = array[last];
-	int current = first - 1, finder = last + 1;
+    int pivot = array[last];
+    int leftPointer = first - 1, rightPointer = last + 1;
 
-	while (current < finder)
-	{
-		do {
-			current++;
-		} while (array[current] < pivot);
+    while (leftPointer < rightPointer)
+    {
+        do {
+            leftPointer++;
+        } while (array[leftPointer] < pivot);
 
-		do {
-			finder--;
-		} while (array[finder] > pivot);
+        do {
+            rightPointer--;
+        } while (array[rightPointer] > pivot);
 
-		if (current < finder)
-		{
-			swap(array, current, finder);
-			print_array(array, size);
-		}
-	}
+        if (leftPointer < rightPointer)
+        {
+            swap(array, leftPointer, rightPointer);
+            print_array(array, size);
+        }
+    }
 
-	return current;
+    return leftPointer;
 }
 
 /**
@@ -57,14 +56,14 @@ int hoare_partition(int *array, int first, int last, int size)
  */
 void qs(int *array, ssize_t first, ssize_t last, int size)
 {
-	ssize_t position = 0;
+    ssize_t position = 0;
 
-	if (first < last)
-	{
-		position = hoare_partition(array, first, last, size);
-		qs(array, first, position, size);
-		qs(array, position + 1, last, size);
-	}
+    if (first < last)
+    {
+        position = hoare_partition(array, first, last, size);
+        qs(array, first, position, size);
+        qs(array, position + 1, last, size);
+    }
 }
 
 /**
@@ -74,7 +73,7 @@ void qs(int *array, ssize_t first, ssize_t last, int size)
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
-	qs(array, 0, size - 1, size);
+    if (!array || size < 2)
+        return;
+    qs(array, 0, size - 1, size);
 }
